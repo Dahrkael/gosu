c3ee27e9755c7d4316459aefa4f5003d1af893a8
Dahrkael <dark_wolf_warrior@hotmail.com>
2016-12-06 19:38:37 +0100

    + added csharp stuff (cppsharp script, binaries, tutorial in C#, etc)

---
+++ csharp/GosuNETer.cs
@@ -0,0 +1,151 @@
+﻿using CppSharp;
+using CppSharp.AST;
+using CppSharp.Passes;
+using System;
+using System.IO;
+
+namespace Gosu.NETer
+{
+    class Gosu : ILibrary
+    {
+        public void Setup(Driver driver)
+        {
+            var parserOptions = driver.ParserOptions;
+            var options = driver.Options;
+
+            options.GeneratorKind = CppSharp.Generators.GeneratorKind.CSharp;
+
+            var gosuPath = GetGosuDirectory();
+            var includePath = Path.Combine(gosuPath, "");
+            var libraryPath = Path.Combine(gosuPath, "");
+            options.LibraryName = "Gosu";
+            options.SharedLibraryName = "Gosu.dll";
+            //options.Headers.Add(@"Gosu\TR1.hpp");
+
+            options.Headers.Add(@"Gosu\Audio.hpp");
+            options.Headers.Add(@"Gosu\Graphics.hpp");
+            options.Headers.Add(@"Gosu\GraphicsBase.hpp");
+            options.Headers.Add(@"Gosu\Image.hpp");
+            options.Headers.Add(@"Gosu\Bitmap.hpp");
+            options.Headers.Add(@"Gosu\Font.hpp");
+            options.Headers.Add(@"Gosu\Math.hpp");
+            options.Headers.Add(@"Gosu\Text.hpp");
+            options.Headers.Add(@"Gosu\TextInput.hpp");
+            options.Headers.Add(@"Gosu\Window.hpp");
+            options.Headers.Add(@"Gosu\Color.hpp");
+            options.Headers.Add(@"Gosu\Buttons.hpp");
+            options.Headers.Add(@"Gosu\Input.hpp");
+            options.Headers.Add(@"Gosu\Timing.hpp");
+
+            parserOptions.AddIncludeDirs(includePath);
+            parserOptions.AddLibraryDirs(libraryPath);
+            options.OutputDir = "Gosu.NET";
+            options.CommentPrefix = "//";
+            options.GenerateDefaultValuesForArguments = true;
+            options.OutputNamespace = "GosuNET";
+            options.IncludePrefix = "Gosu/";
+
+            if (options.IsCLIGenerator)
+            {
+                if (Directory.Exists(@"..\..\..\..\Gosu.NET-CLI"))
+                {
+                    options.OutputDir = @"..\..\..\..\Gosu.NET-CLI";
+                }
+            }
+            if (options.IsCSharpGenerator)
+            {
+                if (Directory.Exists(@"..\..\..\..\Gosu.NET"))
+                {
+                    options.OutputDir = @"..\..\..\..\Gosu.NET";
+                }
+            }
+        }
+
+        public void SetupPasses(Driver driver)
+        {
+            driver.Context.TranslationUnitPasses.RenameDeclsUpperCase(RenameTargets.Any);
+            driver.Context.TranslationUnitPasses.AddPass(new FunctionToInstanceMethodPass());
+            /*
+            driver.Context.TranslationUnitPasses.RemovePrefix("SDL_");
+            driver.Context.TranslationUnitPasses.RemovePrefix("SCANCODE_");
+            driver.Context.TranslationUnitPasses.RemovePrefix("SDLK_");
+            driver.Context.TranslationUnitPasses.RemovePrefix("KMOD_");
+            driver.Context.TranslationUnitPasses.RemovePrefix("LOG_CATEGORY_");
+            */
+        }
+
+        public void Preprocess(Driver driver, ASTContext ctx)
+        {
+            ctx.IgnoreHeadersWithName("AutoLink.hpp");
+            ctx.IgnoreHeadersWithName("Directories.hpp");
+            ctx.IgnoreHeadersWithName("Fwd.hpp");
+            ctx.IgnoreHeadersWithName("Gosu.hpp");
+            //ctx.IgnoreHeadersWithName("GraphicsBase.hpp");
+            ctx.IgnoreHeadersWithName("ImageData.hpp");
+            ctx.IgnoreHeadersWithName("IO.hpp");
+            //ctx.IgnoreHeadersWithName("Math.hpp");
+            ctx.IgnoreHeadersWithName("Platform.hpp");
+            ctx.IgnoreHeadersWithName("Sockets.hpp");
+            ctx.IgnoreHeadersWithName("TR1.hpp");
+            ctx.IgnoreHeadersWithName("Utility.hpp");
+            ctx.IgnoreHeadersWithName("Version.hpp");
+            /*
+            ctx.IgnoreEnumWithMatchingItem("SDL_FALSE");
+            ctx.IgnoreEnumWithMatchingItem("DUMMY_ENUM_VALUE");
+
+            ctx.SetNameOfEnumWithMatchingItem("SDL_SCANCODE_UNKNOWN", "ScanCode");
+            ctx.SetNameOfEnumWithMatchingItem("SDLK_UNKNOWN", "Key");
+            ctx.SetNameOfEnumWithMatchingItem("KMOD_NONE", "KeyModifier");
+            ctx.SetNameOfEnumWithMatchingItem("SDL_LOG_CATEGORY_CUSTOM", "LogCategory");
+
+            ctx.GenerateEnumFromMacros("InitFlags", "SDL_INIT_(.*)").SetFlags();
+            ctx.GenerateEnumFromMacros("Endianness", "SDL_(.*)_ENDIAN");
+            ctx.GenerateEnumFromMacros("InputState", "SDL_RELEASED", "SDL_PRESSED");
+            ctx.GenerateEnumFromMacros("AlphaState", "SDL_ALPHA_(.*)");
+            ctx.GenerateEnumFromMacros("HatState", "SDL_HAT_(.*)")
+
+            ctx.IgnoreEnumWithMatchingItem("SDL_ENOMEM");
+            ctx.IgnoreFunctionWithName("SDL_Error");
+            */
+        }
+
+        public void Postprocess(Driver driver, ASTContext ctx)
+        {
+            /*
+            ctx.SetNameOfEnumWithName("PIXELTYPE", "PixelType");
+            ctx.SetNameOfEnumWithName("BITMAPORDER", "BitmapOrder");
+            ctx.SetNameOfEnumWithName("PACKEDORDER", "PackedOrder");
+            ctx.SetNameOfEnumWithName("ARRAYORDER", "ArrayOrder");
+            ctx.SetNameOfEnumWithName("PACKEDLAYOUT", "PackedLayout");
+            ctx.SetNameOfEnumWithName("PIXELFORMAT", "PixelFormats");
+            ctx.SetNameOfEnumWithName("assert_state", "AssertState");
+            ctx.SetClassBindName("assert_data", "AssertData");
+            ctx.SetNameOfEnumWithName("eventaction", "EventAction");
+            ctx.SetNameOfEnumWithName("LOG_CATEGORY", "LogCategory");
+            */
+        }
+
+        public static string GetGosuDirectory()
+        {
+            var directory = @"H:\Proyectos\gosu";
+            //var directory = string.Format(@"{0}\..\..\..\gosu", Directory.GetCurrentDirectory());
+
+            if (Directory.Exists(directory))
+            {
+                return directory;
+            }
+            else
+            {
+                throw new Exception("gosu directory for project was not found");
+            }
+        }
+
+        static class Program
+        {
+            public static void Main(string[] args)
+            {
+                ConsoleDriver.Run(new Gosu());
+            }
+        }
+    }
+}
---
+++ csharp/example.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GosuNET.Gosu;
+using GosuTutorial;
+
+namespace GosuTutorial
+{
+    enum ZOrder
+    {
+        zBackground,
+        zStars,
+        zPlayer,
+        zUI
+    };
+
+    public class Star
+    {
+        Image image;
+        List<Image> animation;
+        Color color;
+        double posX, posY;
+
+
+        public Star(List<Image> animation)
+        {
+            this.animation = animation;
+            image = new Image("media/star1.png");
+            double red = GosuNET.Gosu.Math.Random(40, 255);
+            double green = GosuNET.Gosu.Math.Random(40, 255);
+            double blue = GosuNET.Gosu.Math.Random(40, 255);
+            color = new Color(255, (byte)red, (byte)green, (byte)blue);
+
+            posX = GosuNET.Gosu.Math.Random(0, 640);
+            posY = GosuNET.Gosu.Math.Random(0, 480);
+        }
+
+        public double x() { return posX; }
+        public double y() { return posY; }
+
+        public void draw()
+        {
+            //Image image = animation[(int)(Timing.Milliseconds() / 100 % animation.Count)];
+            image.Draw(posX - image.Width / 2.0, posY - image.Height / 2.0, (double)ZOrder.zStars, 1, 1, color, (AlphaMode)1);
+        }
+    }
+
+    public class Player
+    {
+        Image image;
+        Sample beep;
+        double posX, posY, velX, velY, angle;
+        UInt32 score;
+
+        public Player()
+        {
+            image = new Image("media/Starfighter.bmp");
+            beep = new Sample("media/Beep.wav");
+            posX = posY = velX = velY = angle = 0;
+            score = 0;
+        }
+
+        public UInt32 getScore()
+        {
+            return score;
+        }
+
+        public void warp(double x, double y)
+        {
+            posX = x;
+            posY = y;
+        }
+
+        public void turnLeft()
+        {
+            angle -= 4.5;
+        }
+
+        public void turnRight()
+        {
+            angle += 4.5;
+        }
+
+        public void accelerate()
+        {
+            velX += GosuNET.Gosu.Math.OffsetX(angle, 0.5);
+            velY += GosuNET.Gosu.Math.OffsetY(angle, 0.5);
+        }
+
+        public void move()
+        {
+            posX = GosuNET.Gosu.Math.Wrap(posX + velX, 0.0, 640.0);
+            posY = GosuNET.Gosu.Math.Wrap(posY + velY, 0.0, 480.0);
+
+            velX *= 0.95;
+            velY *= 0.95;
+        }
+
+        public void draw()
+        {
+            image.DrawRot(posX, posY, (double)ZOrder.zPlayer, angle, 0.5, 0.5, 1, 1, Color.WHITE, 0);
+        }
+
+        public void collectStars(ref List<Star> stars)
+        {
+            int amount = stars.RemoveAll(s => GosuNET.Gosu.Math.Distance(posX, posY, s.x(), s.y()) < 35);
+            if (amount > 0)
+            {
+                score += (uint)(amount * 10);
+                beep.Play();
+            }
+        }
+    }
+
+class GameWindow : Window
+{
+    Image backgroundImage;
+    List<Image> starAnim;
+    Font font;
+
+    Player player;
+    List<Star> stars;
+
+
+    public GameWindow() : base(640, 480)
+    {
+        font = new Font(20);
+        SetCaption("Gosu Tutorial Game");
+
+        backgroundImage = new Image("media/Space.png", (uint)ImageFlags.IfTileable);
+        //starAnim = loadTiles("media/Star.png", 25, 25);
+        stars = new List<Star>();
+        player = new Player();
+        player.warp(320, 240);
+    }
+
+    public override void Update()
+    {
+        if (Input.Down(ButtonName.KbLeft) || Input.Down(ButtonName.GpLeft))
+            player.turnLeft();
+        if (Input.Down(ButtonName.KbRight) || Input.Down(ButtonName.GpRight))
+            player.turnRight();
+        if (Input.Down(ButtonName.KbUp) || Input.Down(ButtonName.GpButton0))
+            player.accelerate();
+
+        player.move();
+        player.collectStars(ref stars);
+
+        if (GosuNET.Gosu.Math.Random(0, 30) > 20 && stars.Count < 25)
+        stars.Add(new Star(starAnim));
+    }
+
+    public override void Draw()
+    {
+        player.draw();
+        backgroundImage.Draw(0, 0, (double)ZOrder.zBackground, 1, 1, Color.WHITE, 0);
+        stars.ForEach(s => s.draw());
+        font.Draw("Score: " + player.getScore(), 10, 10, (double)ZOrder.zUI, 1, 1, Color.YELLOW, 0);
+    }
+
+    public override void ButtonDown(Button btn)
+    {
+        //if (btn == ButtonName.KbEscape) { }
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GameWindow window = new GameWindow();
+        window.Show();
+    }
+}
+}
